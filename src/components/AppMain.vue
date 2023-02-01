aa71c1d7afda80bc82d7591ab28fc5a4ce5c719e
dav100ide <oreglio.dev@gmail.com>
2023-02-01 16:40:22 +0100

    stampo in console response.data con axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,7 +3,16 @@
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+   created() {
+      axios
+         .get('http://127.0.0.1:8000/api/projects') //
+         .then((response) => {
+            console.log(response.data);
+         });
+   },
+};
 </script>
 
 <style lang="scss" scoped></style>
